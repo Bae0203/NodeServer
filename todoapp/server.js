5d682ae4953548ed6eba93165b5cf268aabdde29
bae0203 <gmbae06@dgsw.hs.kr>
2023-04-14 10:34:03 +0900

    ADD :: Learn Get

---
+++ todoapp/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.listen(8080, function () {
+  console.log("listening on 8080");
+});
+
+/**
+ * 누군가가 /pet으로 방문을 하면
+ * pet에 관련된 안내문을 띄워주자
+ */
+
+//요청 = req, 응답 = res
+app.get("/pet", function (요청, 응답) {
+  응답.send("펫 관련 페이지입니다.");
+});
+
+app.get("/beauty", function (req, res) {
+  res.send("화장품 관련 페이지입니다.");
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
