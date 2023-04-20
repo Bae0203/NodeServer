0eaa1dcb74ecb5eb4a5557d0af99790e75dbca3e
bae0203 <gmbae06@dgsw.hs.kr>
2023-04-20 21:33:40 +0900

    ADD :: Post

--- todoapp/server.js
+++ todoapp/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(8080, function () {
   console.log("listening on 8080");
 });
@@ -29,4 +31,5 @@ app.get("/write", (req, res) => {
 
 app.post("/add", (req, res) => {
   res.send("전송완료");
+  console.log(req.body);
 });
