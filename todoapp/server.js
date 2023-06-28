009e81345faba390892dfa953547f2a4ee0b2003
bae0203 <gmbae06@dgsw.hs.kr>
2023-06-28 14:30:51 +0900

    Fix :: Cors

--- todoapp/server.js
+++ todoapp/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+const cors = require("cors");
+app.use(cors());
+
 // let MongoClient = require("mongodb").MongoClient;
 
 const url =
@@ -20,13 +23,12 @@ mongoose
   .connect(url)
   .then(() => {
     console.log("MongoDB에 연결되었습니다.");
-    Connection();
   })
   .catch((error) => {
     console.error("MongoDB 연결 오류:", error);
   });
 
-function Connection() {
+function Connection(props) {
   const Post = mongoose.model("post", postShema);
   Post.find()
     .then((posts) => {
@@ -41,11 +43,33 @@ function Connection() {
   let month = today.getMonth() + 1; // 월
   let date = today.getDate(); // 날짜
   const NewPost = new Post({
-    title: "테스트1",
-    context: "테스트22222",
+    title: props.title,
+    context: props.context,
     date: `${year} / ${month} / ${date}`,
   });
   NewPost.save()
-    .then((e) => console.log(e))
+    .then((_) => console.log("성공적으로 저장했습니다."))
     .catch((e) => console.log("Error !!! : ", e));
 }
+
+app.post("/add", (req, rep) => {
+  Connection({ title: req.body.title, context: req.body.context });
+  rep.send({ text: "저장되었습니다.", value: req.body });
+});
+
+app.get("/view", (req, rep) => {
+  let list = [];
+  const Post = mongoose.model("post", postShema);
+  Post.find()
+    .then((posts) => {
+      list = [...posts];
+      console.log("모든 문서:", posts);
+      rep.send({ list: list });
+    })
+    .catch((error) => {
+      console.error("문서 조회 오류:", error);
+      rep.send({ list: list });
+    });
+});
+
+app.listen(8080);
