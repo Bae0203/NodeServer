bfc9c5dbb6012a1a918cbf3cc11a5091b697bf52
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-03 23:51:02 +0900

    FIX :: File Separation

--- mysqlExample/server.js
+++ mysqlExample/server.js
@@ -5,16 +5,10 @@ app.use(express.json());
 app.use(cors());
 const postRoutes = require("./routes/post");
 const commentRoutes = require("./routes/comment");
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
 
 app.listen(8080, () => {
   console.log("서버 시작");
 });
-
-// app.get("/", function (req, res) {
-//   res.send("hello NodeJs");
-// });
 
 app.use("/post", postRoutes);
 
