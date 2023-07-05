4a645cd0be34c6d906c2d926146df06d5127829a
GyoungMin <101560583+Bae0203@users.noreply.github.com>
2023-07-06 00:16:06 +0900

    ADD :: Password Hashed

--- mysqlExample/routes/auth.js
+++ mysqlExample/routes/auth.js
@@ -1,6 +1,13 @@
 const authRoutes = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-authRoutes.post("/signin", function (req, res) {});
+authRoutes.post("/signup", async function (req, res) {
+  const { name, id, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const token = jwt.sign({ id }, "your_secret_key");
+
+  res.json({ token });
+});
 
 module.exports = authRoutes;
