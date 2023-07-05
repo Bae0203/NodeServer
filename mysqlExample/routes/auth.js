69b8a1de278c59ec137edba11747aed6d84b3b0a
GyoungMin <101560583+Bae0203@users.noreply.github.com>
2023-07-05 23:51:25 +0900

    ADD :: module

--- mysqlExample/routes/auth.js
+++ mysqlExample/routes/auth.js
@@ -1,5 +1,6 @@
 const authRoutes = express.Router();
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 authRoutes.post("/signin", function (req, res) {});
 
 module.exports = authRoutes;
