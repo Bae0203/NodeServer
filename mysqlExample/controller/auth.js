1d2922caf21f9efe929cb7a7bb7ca92dcc2cd6ab
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-06 23:02:51 +0900

    ADD :: SignUp

--- mysqlExample/controller/auth.js
+++ mysqlExample/controller/auth.js
@@ -2,6 +2,14 @@ let connection = require("../db/Connect");
 
 function SignIn(props) {}
 
-function SignUp(props) {}
+function SignUp(props) {
+  let Query = `INSERT INTO UserInfo (userId, userPassword, userName) VALUES ("${props.id}", "${props.password}",b"${props.name}");`;
+  connection.query(Query, function (err, result) {
+    if (err) {
+      console.log("Error", err);
+    }
+    console.log("Success", result);
+  });
+}
 
 module.exports = { SignIn, SignUp };
--- mysqlExample/routes/auth.js
+++ mysqlExample/routes/auth.js
@@ -1,11 +1,16 @@
+const express = require("express");
 const authRoutes = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const { SignUp, SignIn } = require("../controller/auth");
+
 authRoutes.post("/signup", async function (req, res) {
   const { name, id, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const token = jwt.sign({ id }, "your_secret_key");
+  SignUp({ id: id, password: hashedPassword, name: name });
+
+  const token = jwt.sign({ id, name }, "your_secret_key");
 
   res.json({ token });
 });
