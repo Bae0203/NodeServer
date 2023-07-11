0b1f26366275458a2a60bd7475c8cb9bda12df3b
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-11 21:27:40 +0900

    FIX :: Auth

--- AuthExample/routes/auth.js
+++ AuthExample/routes/auth.js
@@ -2,25 +2,11 @@ const express = require("express");
 const authRoutes = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const { SignUp, SignIn, IdCheck } = require("../controller/auth");
 
-authRoutes.post("/signin", async function (req, res) {
-  const { id, password } = req.body;
-  SignIn({ id: id, password: password, res: res });
-});
+authRoutes.post("/signin", async function (req, res) {});
 
-authRoutes.post("/signup", async function (req, res) {
-  const { name, id, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
+authRoutes.post("/signup", async function (req, res) {});
 
-  SignUp({ id: id, password: hashedPassword, name: name });
-  const token = jwt.sign({ id }, "login_token");
-  res.json({ token });
-});
-
-authRoutes.post("/signup/idcheck", async function (req, res) {
-  const { id } = req.body;
-  IdCheck({ id: id, res: res });
-});
+authRoutes.post("/signup/usercheck", async function (req, res) {});
 
 module.exports = authRoutes;
